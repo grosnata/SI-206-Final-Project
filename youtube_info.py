83564391e81c9b69011ffe5c67960301689d1356
janeuma <33671764+janeuma@users.noreply.github.com>
2019-04-17 13:39:32 -0400

    Delete youtube_info.py

--- youtube_info.py
+++
@@ -1,7 +0,0 @@
-#youtube credentials
-api_key= "AIzaSyALtuOrfBRq3Sc58WbHITyZFA2at153JbE"
-client_id= "38459285321-l5pvam87vd4l8qqv9oaumf1tamftvb5a.apps.googleusercontent.com"
-client_secret= "KbYXmjZIU0dEuNkVVg43_NEf"
-auth_uri= "https://accounts.google.com/o/oauth2/auth"
-token_uri= "https://oauth2.googleapis.com/token"
-redirect_uris= ["urn:ietf:wg:oauth:2.0:oob","http://localhost"]
